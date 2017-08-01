e8d864b4f223ac8ab9118436aee6ed9a2481cc4e
Nirgalz <znirgal@gmail.com>
2017-08-01 10:19:33 +0200

    cleanup and stuff

--- renderer.js
+++ renderer.js
@@ -6,32 +6,9 @@ const ipc = require('electron').ipcRenderer
 const Datastore = require('nedb')
 const db = new Datastore({ filename: 'data/savefile', autoload: true });
 
-const printPDFBtn = document.getElementById('print-pdf')
 const test = document.getElementById('test')
 
 
-//
-// printPDFBtn.addEventListener('click', function () {
-//     this.style.visibility = 'hidden'
-//
-//
-//     if (data.edited === 0) {
-//         db.insert(data, function (err, newDoc) {
-//             console.log(newDoc)
-//         });
-//     } else if (data.edited === 1) {
-//         db.update({ _id: data._id }, data, {}, function (err, numReplaced) {
-//             console.log(numReplaced + ' rows updated')
-//         });
-//     }
-//     // ipc.send('print-to-pdf')
-// })
-
-
-
-// test.addEventListener('click', function (event) {
-//     ipc.send('test')
-// })
 
 ipc.on('wrote-pdf', function (event, path) {
   const message = `Wrote PDF to: ${path}`
